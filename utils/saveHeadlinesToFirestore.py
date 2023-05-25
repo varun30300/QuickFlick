23837ee32e7095d2ced04f87475d9b8368f9b902
varun30300 <varunpne@gmail.com>
2023-05-24 21:42:03 -0400

    v_1 of the saving news to firebase is complete

--- utils/saveHeadlinesToFirestore.py
+++ utils/saveHeadlinesToFirestore.py
@@ -9,33 +9,68 @@ KEY = os.getenv('NEWS_API_KEY')
 cred = credentials.Certificate("quickflick-19fdd-11e52bcd6a8b.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-doc_ref = db.collection(u'')
-
 
 # reading the first page 
 curr_page = "https://newsdata.io/api/1/news?apikey="+ KEY +"&country=ca&language=en"
 
 page_no = 1
-while page_no <= 10 : 
+while page_no <= 1 : 
     response = requests.get(curr_page).json()
 
     for article in response['results']:
         title = article['title']
         link = article['link']
-        keyWords = article['keywords']
+        keywords = article['keywords']
         author = article['creator']
         description = article['description']
         content = article['content']
-        pubDate = article['pubDate']
+        pubDate = article['pubDate'].split(' ')[0]   # yyyy-mm-dd
         image_url = article['image_url']
         source_id = article['source_id']
         category = article['category']
         country = article['country']
         language = article['language']
+        docID = str(hash(link))
 
+        data = {
+            "title" : title,
+            "link" : link ,
+            "keywords" : keywords ,
+            "author" : author, 
+            "description" : description, 
+            "content" : content, 
+            "pubDate" : pubDate, 
+            "image_url" : image_url, 
+            "source_id" : source_id, 
+            "category" : category, 
+            "country" : country, 
+            "language" : language
+        }
+
+
+        print(docID)
+        print(pubDate)
+        collection = db.collection(pubDate)
+        doc = collection.document(docID)
+        
+
+    # if in pubDate collection if there is a doc with id link, ignore
+        if (doc.get().exists):
+            print("Old article, doing nothing")
+        else :
+            print("New article, adding it to the database")
+            doc.set(data)
+            print("added")
+
+
+    
+    # else in pubdate collection add the link doc 
 
     page_no += 1
-    curr_page = "https://newsdata.io/api/1/news?apikey="+ KEY +"&country=ca&language=en&page=" + response['nextPage']
+    if  response['nextPage'] : 
+        curr_page = "https://newsdata.io/api/1/news?apikey="+ KEY +"&country=ca&language=en&page=" + response['nextPage']
+    else :
+        break
 
 
 # iterate and check if link for that date already exists, if yes ignore , if no add that to the news 
